5292b530e4217696018c131ffa3025127d92a877
Pouria SotoudehSharifi <181016223+kianeutron@users.noreply.github.com>
2025-09-16 12:12:55 +0200

    initial commit

---
+++ src/main/java/org/example/quizzapp/Launcher.java
@@ -0,0 +1,9 @@
+package org.example.quizzapp;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
